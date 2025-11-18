308ce487840dfd5121f3956bc13c6e8e0d18e669
MorganMcmillan <mcmillan.a.morgan@gmail.com>
2025-11-18 11:54:37 -0700

    Misc: add extra findBy methods to repositories

--- src/main/java/com/CampusJobBoardSystem/repository/UserRepository.java
+++ src/main/java/com/CampusJobBoardSystem/repository/UserRepository.java
@@ -1,12 +1,17 @@
 package com.CampusJobBoardSystem.repository;
 
+import com.CampusJobBoardSystem.model.Role;
 import com.CampusJobBoardSystem.model.User;
+import com.CampusJobBoardSystem.model.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByStatus(UserStatus status);
+    List<User> findByRole(Role role);
     Optional<User> findByEmail(String email);
 }
